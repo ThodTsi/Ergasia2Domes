c6d65ceb02edf4e9db25bb1f7ad726d2ed81c5c5
ThodTsi <teotsi4002@gmail.com>
2023-12-14 16:26:09 +0200

    Update Influenza_k.java

--- Influenza_k.java
+++ Influenza_k.java
@@ -7,23 +7,23 @@ public class Influenza_k {
 
     public static void main(String args[]) {
 
-        try{
+        try {
             Scanner in = new Scanner(System.in);
-            //System.out.print("Give file name: ");
-            //String filename = in.nextLine();
+            // System.out.print("Give file name: ");
+            // String filename = in.nextLine();
             int numberOfLines = 0; // gia to megethos tou pinaka
             BufferedReader reader = new BufferedReader(new FileReader("inf.txt"));
             String line;
             while ((line = reader.readLine()) != null) {
                 numberOfLines++;
             }
-            
-            City[] city = new City[numberOfLines + 1];
-            
-            readFile("inf.txt", city);
 
-            for(City x: city){
-                if (x != null){
+            City[] city = new City[numberOfLines + 1];
+
+            readFile("inf.txt", city);
+            System.out.println(city[1].compareTo(city[2]));
+            for (City x : city) {
+                if (x != null) {
                     System.out.println(x.toString());
                     System.out.println(x.calculateDensity());
                 }
@@ -32,76 +32,83 @@ public class Influenza_k {
             System.out.println(city.length);
 
             City[] citySort = heapSort(city);
-            
-            for(City x: citySort){
-                if (x!= null){
+
+            for (City x : citySort) {
+                if (x != null) {
                     System.out.println(x.toString());
                 }
             }
             System.out.println();
-            
+
             /*
-            System.out.print("Enter number of least infected cities per 50.000 citizens to be displayed: ");
-            int k = in.nextInt();
-            if (k >= 1 & k <= citySort.length) {
-                System.out.println("The top " + k + " cities are:");
-                for (int i = 0; i < k; i++) {
-                    System.out.println(citySort[i].getName());
-                }
-            }else{
-                System.out.println("This number is not valid");
-            } */
-        }catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("error oppening the file...");
+             * System.out.
+             * print("Enter number of least infected cities per 50.000 citizens to be displayed: "
+             * );
+             * int k = in.nextInt();
+             * if (k >= 1 & k <= citySort.length) {
+             * System.out.println("The top " + k + " cities are:");
+             * for (int i = 0; i < k; i++) {
+             * System.out.println(citySort[i].getName());
+             * }
+             * }else{
+             * System.out.println("This number is not valid");
+             * }
+             */
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("error oppening the file...");
         }
 
     }
 
-    public static void readFile(String filePath, City [] c){
+    public static void readFile(String filePath, City[] c) {
 
-        try{
-            int id = 0, population = 0 , inf_cases = 0;
+        try {
+            int id = 0, population = 0, inf_cases = 0;
             String name = "";
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
-            StringBuilder currentLine = new StringBuilder(); //gia na mporoym na kratame kai to kathe line pera apo kathe character
-            int ch ; // character se int giati o BufferedReader diavazei se ASCII
-            int startIndex = 0, endIndex = 0; //start kai end gia na "kopsoyme" to line se kommatia kai na paroyme tis plhrofories
-            int whiteSpace = 0; //gia na xwristei to line opote briskei keno
-            int n = 0; // metraei se poio line eimaste kai xrhsimopoietai ws deikths ston pinaka gia ayto kai -1
+            StringBuilder currentLine = new StringBuilder(); // gia na mporoym na kratame kai to kathe line pera apo
+                                                             // kathe character
+            int ch; // character se int giati o BufferedReader diavazei se ASCII
+            int startIndex = 0, endIndex = 0; // start kai end gia na "kopsoyme" to line se kommatia kai na paroyme tis
+                                              // plhrofories
+            int whiteSpace = 0; // gia na xwristei to line opote briskei keno
+            int n = 0; // metraei se poio line eimaste kai xrhsimopoietai ws deikths ston pinaka gia
+                       // ayto kai -1
 
             while ((ch = reader.read()) != -1) {
-                if ((char) ch == '\n'){
-                    inf_cases = Integer.parseInt(currentLine.toString().trim().substring(startIndex, endIndex-1));
+                if ((char) ch == '\n') {
+                    inf_cases = Integer.parseInt(currentLine.toString().trim().substring(startIndex, endIndex - 1));
                     n++;
-                    endIndex = startIndex = 0;         //allazei line, jana arxikopoihsh
-                    currentLine.setLength(0); //"katharizoyme to line"
-                    whiteSpace =-1; //-1 giati diavazei ena parapanw whiteSpace otan allazei line
+                    endIndex = startIndex = 0; // allazei line, jana arxikopoihsh
+                    currentLine.setLength(0); // "katharizoyme to line"
+                    whiteSpace = -1; // -1 giati diavazei ena parapanw whiteSpace otan allazei line
 
                     City city = new City(id, name, population, inf_cases);
-                    c[n] = city;  
-                    heapify(c, n);                  
-                }else{
+                    c[n] = city;
+                    heapify(c, n);
+                } else {
                     endIndex++;
                     currentLine.append((char) ch);
                 }
 
-                if (Character.isWhitespace((char) ch)){
-                    whiteSpace ++;
-                    if (whiteSpace == 1){
-                        id = Integer.parseInt(currentLine.toString().trim().substring(startIndex, endIndex-1));
+                if (Character.isWhitespace((char) ch)) {
+                    whiteSpace++;
+                    if (whiteSpace == 1) {
+                        id = Integer.parseInt(currentLine.toString().trim().substring(startIndex, endIndex - 1));
                         startIndex = endIndex;
-                    }else if(whiteSpace == 2){
-                        name = currentLine.toString().trim().substring(startIndex, endIndex-1);
+                    } else if (whiteSpace == 2) {
+                        name = currentLine.toString().trim().substring(startIndex, endIndex - 1);
                         startIndex = endIndex;
-                    }else if(whiteSpace == 3){
-                        population = Integer.parseInt(currentLine.toString().trim().substring(startIndex, endIndex-1));
+                    } else if (whiteSpace == 3) {
+                        population = Integer
+                                .parseInt(currentLine.toString().trim().substring(startIndex, endIndex - 1));
                         startIndex = endIndex;
                     }
-                    
+
                 }
             }
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
             System.out.println("error");
         }
@@ -127,8 +134,8 @@ public class Influenza_k {
     }
 
     public static City[] heapSort(City[] c) {
-        City [] temp = new City[c.length];
-        for (int i = c.length -1 ; i > 0; i--) {
+        City[] temp = new City[c.length];
+        for (int i = c.length - 1; i > 0; i--) {
             temp[i] = c[1];
             c[1] = c[i];
             heapify(c, i);
