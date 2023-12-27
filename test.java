89d5468d6c3ff35e318f9641043e00c25168a652
ThodTsi <teotsi4002@gmail.com>
2023-12-27 19:43:16 +0200

    Update test.java

--- test.java
+++ test.java
@@ -4,30 +4,29 @@ import java.io.IOException;
 
 public class test extends Influenza_k {
     public static void main(String[] args) {
-        try {
-            PQ pq = new PQ();
-            BufferedReader reader = new BufferedReader(new FileReader("inf.txt"));
-            String line;
-            int numberOfLines = 0;
-            while ((line = reader.readLine()) != null) {
-                numberOfLines++;
-            }
-            City[] city = new City[numberOfLines + 1];
-            readFile("inf.txt", city);
-            for (City c : city) {
-                if (c != null) {
-                    pq.insert(c);
-                }
-            }
-            System.out.println(pq.getMin().getName());
-            System.out.println(pq.min().getName());
-            System.out.println(pq.size());
-            System.out.println(pq.getMin().getName());
-            System.out.println(pq.remove(65).getName());
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("error oppening the file...");
+        PQ pq = new PQ();
+        pq.insert(new City(17, "Vienna", 1975000, 2200));
+        pq.insert(new City(38, "Amsterdam", 921402, 1050));
+        pq.insert(new City(65, "Paris", 2102650, 3504));
+        pq.insert(new City(47, "Athens", 698567, 830));
+        pq.insert(new City(12, "Thessaloniki", 319045, 413));
+        for (int i = 1; i <= pq.size(); i++) {
+            System.out.println(pq.heap[i].toString());
         }
+        System.out.println(pq.remove(65).getName());
+        System.out.println(pq.remove(47).getName());
+        System.out.println(pq.size());
+        for (int i = 0; i <= pq.heap.length - 1; i++) {
+            if (pq.heap[i] != null) {
+                System.out.println(pq.heap[i].toString());
+            }
+
+        }
+        // System.out.println(pq.getMin());
+        System.out.println(pq.min());
+        System.out.println(pq.remove(12).getName());
+        System.out.println(pq.remove(17).getName());
+        System.out.println(pq.remove(38).getName());
 
     }
 }
